10183c472b13e1f8bc5c59a9e2ed04490c8a5545
eliteRaihan <elite.raihan@gmail.com>
2020-07-25 16:29:56 +0700

    add js: performCalculation()

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -82,3 +82,19 @@ function handleOperator(operator) {
         alert('Operator sudah ditetapkan')
     }
 }
+
+function performCalculation() {
+    if (calculator.firstNumber == null || calculator.operator == null) {
+        alert("Anda belum menetapkan operator");
+        return;
+    }
+
+    let result = 0;
+    if (calculator.operator === "+") {
+        result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+    } else {
+        result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber)
+    }
+
+    calculator.displayNumber = result;
+}
